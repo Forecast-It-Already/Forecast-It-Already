0d07c057e30b15be31df2d502833a396720f50c1
Braily Guzman <brailyg@outlook.com>
2025-01-15 14:41:51 -0500

    Boilerplate

---
+++ app/src/fetch.js
@@ -0,0 +1,11 @@
+/*
+    TODO Functions
+
+    - getGeoCoding: This function will be responsible for
+    fetching the server for the latitude and longitude based
+    on a string like "Brooklyn".
+
+    - fetchWeatherData: This function will be responsible
+    for fetching the weather information.
+
+*/
