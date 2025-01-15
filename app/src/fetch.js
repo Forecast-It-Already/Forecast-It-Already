c457d75ba25e9bc9ac7f299e9ec35ff86704e5e2
Athena <athenachang17@gmail.com>
2025-01-15 15:41:43 -0500

    implement geo coding function

--- app/src/fetch.js
+++ app/src/fetch.js
@@ -9,3 +9,26 @@
     for fetching the weather information.
 
 */
+
+// Latitude Longitude API
+export const getGeoCoding = async (name) => {
+    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${name}&count=1&language=en&format=json`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Location Not Found');
+
+        const data = await response.json();
+        const geoData = data.results[0];
+
+        return {
+            name: geoData.name,
+            latitude: geoData.latitude,
+            longitude: geoData.longitude
+        };
+    }
+    catch (err) {
+        console.error(err);
+        return null;
+    };
+};
