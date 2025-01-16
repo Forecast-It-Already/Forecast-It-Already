954c1b64240a62711e10900f93e03f283ed713a2
Athena <athenachang17@gmail.com>
2025-01-16 11:42:57 -0500

    dom-helpers.js: daily function done

--- app/src/constants.js
+++ app/src/constants.js
@@ -23,3 +23,34 @@ export const weatherIcons = {
     96: 'wi wi-thunderstorm',
     99: 'wi wi-thunderstorm',
 };
+
+export const slogan = [
+    {
+        rain: [
+            'If the goose honks high, fair weather. If the goose honks low, foul weather.',
+            'When your joints start to ache, rainy weather is at stake.',
+            'When wooden chairs creak, rain will come within a week.',
+            'When gnats swarm and bite, rain is in sight.',
+        ],
+    },
+    { humidity: 'When pipes smell stronger, it’s going to rain.' },
+    {
+        dry: 'If spiders are many and spinning their webs, the spell will soon be very dry.',
+    },
+    {
+        cold: {
+            name: 'New York',
+            slogan: "It's BRICK outside!",
+        },
+    },
+    {
+        sunset: 'Red sky at night, sailor’s delight; red sky in morning, sailor’s warning.',
+    },
+    { storm: 'When frogs croak loudly, it’s going to rain.' },
+    {
+        snow: 'If a circle forms ‘round the moon, ‘twill rain or snow soon.',
+    },
+    {
+        clear: 'When the morning dew is heavy, rain will not come that day.',
+    },
+];
