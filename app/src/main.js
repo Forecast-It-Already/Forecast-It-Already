8e572a55f8f77122795360b6b7c15bf4478a5b27
Braily Guzman <brailyg@outlook.com>
2025-01-17 16:41:24 -0500

    Implement dark mode / theme change functionality and styling

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,11 @@
 import './events.js';
 import { renderWeatherData } from './dom-helpers.js';
-import { initializeTempUnit } from './storage.js';
+import { initializeTempUnit, initializeTheme } from './storage.js';
 import { getWeatherData } from './fetch.js';
 
 const main = async () => {
+    initializeTheme();
+
     const weatherData = await getWeatherData(
         40.6501,
         -73.94958,
