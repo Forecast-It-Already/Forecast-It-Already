d850f7ef70253b2d0e6783382037a24d49b9dca9
Braily Guzman <brailyg@outlook.com>
2025-01-21 12:03:23 -0500

    Update base and build directory

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // GitHub Pages expects an index.html in the root directory
-  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: '..' },
-  // needed for github pages just put the repo name here
-  base: '/Forecast-It-Already', 
+    // GitHub Pages expects an index.html in the root directory
+    // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+    build: { outDir: './dist' },
+    // needed for github pages just put the repo name here
+    base: '/',
 });
